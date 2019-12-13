53582c95700d34fcdd30d13c512485dc7cfe58ff
LAPTOP-QB576EQ3\Raymond <raymondm@sonnet.digital>
2019-12-13 10:14:59 +0800

    Review page

--- TierraCreative/Controllers/ReviewApprovesController.cs
+++ TierraCreative/Controllers/ReviewApprovesController.cs
@@ -77,8 +77,107 @@ namespace TierraCreative.Controllers
 
             var source = Request.QueryString["source"];
 
+            var review = new ReviewModel();
 
-            return View();
+            switch (source) {
+                case "DRP":
+                    var drp = _context.DRPs.Include(a => a.User).Include(a => a.ReviewedUser)
+                                           .SingleOrDefault(x=>x.DRPId == id);
+                    review = new ReviewModel
+                    {
+                        Id = drp.DRPId,
+                        Source = "DRP",
+                        From = drp.CSN,
+                        To = "",
+                        ISIN = drp.ISIN,
+                        Amount = Convert.ToDouble(drp.DRPAmount),
+                        SubmittedBy = drp.User.UserName,
+                        SubmittedDate = drp.CreatedDate,
+                        ApprovedBy = (drp.ReviewedById != null ? drp.ReviewedUser.UserName : "Pending")
+                    };
+
+                    break;
+                case "AIL":
+                    var ail = _context.AILs.Include(a => a.User).Include(a => a.ReviewedUser)
+                                           .SingleOrDefault(x => x.AILId == id);
+                    review = new ReviewModel {
+                        Id = ail.AILId,
+                        Source = "AIL",
+                        From = ail.FromCSN,
+                        To = ail.ToCSN,
+                        ISIN = ail.ISIN,
+                        Amount = Convert.ToDouble(ail.TransferAmount),
+                        SubmittedBy = ail.User.UserName,
+                        SubmittedDate = ail.CreatedDate,
+                        ApprovedBy = (ail.ReviewedById != null ? ail.ReviewedUser.UserName : "Pending")
+                    };
+
+                    break;
+                case "Supplementary Dividend":
+                    var sP = _context.SupplementaryDividends.Include(a => a.User).Include(a => a.ReviewedUser)
+                                                            .SingleOrDefault(x => x.SDId == id);
+                    review = new ReviewModel
+                    {
+                        Id = sP.SDId,
+                        Source = "Supplementary Dividend",
+                        From = sP.FromCSN,
+                        To = sP.ToCSN,
+                        ISIN = sP.ISIN,
+                        Amount = Convert.ToDouble(sP.TransferAmount),
+                        SubmittedBy = sP.User.UserName,
+                        SubmittedDate = sP.CreatedDate,
+                        ApprovedBy = (sP.ReviewedById != null ? sP.ReviewedUser.UserName : "Pending")
+                    };
+
+                    break;
+            }
+
+            return View(review);
+        }
+
+        [HttpPost]
+        public ActionResult Approve(FormCollection form) {
+            var id = int.Parse(form["Id"]);
+            var source = form["Source"];
+
+            switch (source)
+            {
+                case "DRP":
+                    var drp = _context.DRPs.Include(a => a.User).Include(a => a.ReviewedUser)
+                                           .SingleOrDefault(x => x.DRPId == id);
+
+                    drp.ReviewedById = int.Parse(Session["UserId"].ToString());
+                    drp.ReviewedDate = System.DateTime.Now;
+
+                    _context.Entry(drp).State = EntityState.Modified;
+                    _context.SaveChanges();
+
+                    break;
+                case "AIL":
+                    var ail = _context.AILs.Include(a => a.User).Include(a => a.ReviewedUser)
+                                           .SingleOrDefault(x => x.AILId == id);
+
+                    ail.ReviewedById = int.Parse(Session["UserId"].ToString());
+                    ail.ReviewedDate = System.DateTime.Now;
+
+                    _context.Entry(ail).State = EntityState.Modified;
+                    _context.SaveChanges();
+
+                    break;
+                case "Supplementary Dividend":
+                    var sP = _context.SupplementaryDividends.Include(a => a.User).Include(a => a.ReviewedUser)
+                                                            .SingleOrDefault(x => x.SDId == id);
+
+                    sP.ReviewedById = int.Parse(Session["UserId"].ToString());
+                    sP.ReviewedDate = System.DateTime.Now;
+
+                    _context.Entry(sP).State = EntityState.Modified;
+                    _context.SaveChanges();
+
+                    break;
+            }
+
+            return Redirect("/ra/review");
         }
 
         protected override void Dispose(bool disposing)
