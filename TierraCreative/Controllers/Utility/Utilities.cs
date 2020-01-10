fb0019c65406183242281a20ac5003156aa1183f
John Wendell Unabia <wendell.unabia@gmail.com>
2020-01-10 11:56:21 +0800

    Updated email template

--- TierraCreative/Controllers/Utility/Utilities.cs
+++ TierraCreative/Controllers/Utility/Utilities.cs
@@ -128,9 +128,9 @@ namespace TierraCreative.Controllers.Utility
                     .Replace("[AmountValue]", AmountValue)
                     .Replace("[Timestamp]", Timestamp);
 
-            emailmodel.Subject = subject + "<br /><br />This notification was sent on " + System.DateTime.Now.ToString("dd/MM/yyyy") + " by the CISELECT application.";
-            emailmodel.Body = body;
-            body += " This e-mail was sent on by the CISELECT application.";
+            emailmodel.Subject = subject ;
+            emailmodel.Body = body + "<br /><br />This notification was sent on " + System.DateTime.Now.ToString("dd/MM/yyyy") + " by the CISELECT application.";
+            //body += " This e-mail was sent on by the CISELECT application.";
             emailmodel.IsBodyHtml = true;
 
             var success = emailhelper.SendEmail(emailmodel);
