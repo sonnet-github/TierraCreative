19e4da48e5a9b2e272d1b38943e3e44ff393f285
John Wendell Unabia <wendell.unabia@gmail.com>
2020-01-30 20:22:04 +0800

    Updated email and remove button

--- TierraCreative/Controllers/Utility/Utilities.cs
+++ TierraCreative/Controllers/Utility/Utilities.cs
@@ -61,58 +61,60 @@ namespace TierraCreative.Controllers.Utility
                       .Replace("[TransactionID]", TransactionID)
                       .Replace("[FormName]", FormName)
                       .Replace("[Timestamp]", System.DateTime.Now.ToString("yyyy-MM-dd"));
+            string body = "<style type=\"text/css\">body{font-family:Verdana, Geneva, sans-serif;font-size:13px;line-height:19px;}table {border: solid #666 1px;font-size:13px;line-height:19px;}td {border: solid #666 1px;padding:5px;}</ style >";
 
-            string body = @"The following [FormName] transaction was approved by [FullName] ([Username]): <br />
-                                <table border='1'>
+            body += @"The following [FormName] transaction was approved by [FullName] ([Username]): <br />
+                                <table  border='1' cellspacing='0' cellpadding='0'>
                                     <tr>
-                                        <td>ID</td>
-                                        <td>[TransactionID]</td>
+                                        <td width='160' valign='top'>ID</td>
+                                        <td width='151' valign='top'>[TransactionID]</td>
                                     </tr>
                                     <tr>
-                                        <td>CSN</td>
-                                        <td>[FromCSNValue]</td>
+                                        <td width='160' valign='top'>CSN</td>
+                                        <td width='151' valign='top'>[FromCSNValue]</td>
                                     </tr>
                                     <tr>
-                                        <td>ISIN</td>
-                                        <td>[ISINValue]</td>
+                                        <td width='160' valign='top'>ISIN</td>
+                                        <td width='151' valign='top'>[ISINValue]</td>
                                     </tr>
                                     <tr>
-                                        <td>DRP Amount</td>
-                                        <td>[AmountValue]</td>
+                                        <td width='160' valign='top'>DRP Amount</td>
+                                        <td width='151' valign='top'>[AmountValue]</td>
                                     </tr>
                                     <tr>
-                                        <td>Date</td>
-                                        <td>[Timestamp]</td>
+                                        <td width='160' valign='top'>Date</td>
+                                        <td width='151' valign='top'>[Timestamp]</td>
                                     </tr>
                                 </table>";
 
             if (FormName == "AIL" || FormName == "Supplementary Dividend")
             {
-                body = @"The following [FormName] transaction was approved by [FullName] ([Username]): <br />
-                                <table border='1'>
+                body = "<style type=\"text/css\">body{font-family:Verdana, Geneva, sans-serif;font-size:13px;line-height:19px;}table {border: solid #666 1px;font-size:13px;line-height:19px;}td {border: solid #666 1px;padding:5px;}</ style >";
+                body += "<p>The following <strong>[FormName]</strong> transaction was approved by  <strong>[FullName]</strong>  (  <strong>[Username]</strong>  ):</p>";
+                body += @"<table border='1' cellspacing='0' cellpadding='0'>
                                     <tr>
-                                        <td>ID</td>
-                                        <td>[TransactionID]</td>
+                                        <td width='160' valign='top'>ID</td>
+                                        <td width='151' valign='top'>[TransactionID]</td>
                                     </tr>
                                     <tr>
-                                        <td>From CSN</td>
-                                        <td>[FromCSNValue]</td>
+                                        <td width='160' valign='top'>From CSN</td>
+                                        <td width='151' valign='top'>[FromCSNValue]</td>
                                     </tr>
                                     <tr>
-                                        <td>To CSN</td>
-                                        <td>[ToCSNValue]</td>
+                                        <td width='160' valign='top'>To CSN</td>
+                                        <td width='151' valign='top'>[ToCSNValue]</td>
                                     </tr>
                                     <tr>
-                                        <td>ISIN</td>
-                                        <td>[ISINValue]</td>
+                                        <td width='160' valign='top'>ISIN</td>
+                                        <td width='151' valign='top'>[ISINValue]</td>
                                     </tr>
                                     <tr>
-                                        <td>Transfer Amount</td>
-                                        <td>[AmountValue]</td>
+                                        <td width='160' valign='top'>Transfer Amount</td>
+                                        <td width='151' valign='top'>[AmountValue]</td>
                                     </tr>
                                     <tr>
-                                        <td>Date</td>
-                                        <td>[Timestamp]</td>
+                                        <td width='160' valign='top'>Date</td>
+                                        <td width='151' valign='top'>[Timestamp]</td>
                                     </tr>
                                 </table>";
             }
@@ -129,7 +131,8 @@ namespace TierraCreative.Controllers.Utility
                     .Replace("[Timestamp]", Convert.ToDateTime(Timestamp).ToString("yyyy-MM-dd"));
 
             emailmodel.Subject = subject ;
-            emailmodel.Body = body + "<br /><br />This notification was sent on " + System.DateTime.Now.ToString("yyyy-MM-dd") + " by the CISELECT application.";
+            emailmodel.Body = body + "<p>  <em>This notification was sent on  <strong style=\"color:#f30;\">" + System.DateTime.Now.ToString("yyyy-MM-dd") + "</strong> by the CISELECT application.</em></p>";
+            emailmodel.Body += "<p><img src=\"http://tierracreative.clientpreview.agency/Images/Logo.jpg\" width=\"240\" height=\"60\"></p>";
             //body += " This e-mail was sent on by the CISELECT application.";
             emailmodel.IsBodyHtml = true;
 
@@ -168,16 +171,15 @@ namespace TierraCreative.Controllers.Utility
             var subject = @"CISELECT – Forgotten Password";
             if (HttpContext.Current.Request.Url.AbsoluteUri.ToLower().IndexOf("/admin") != -1)
                 subject = "CISELECT – Admin Forgotten Password";
-
-            var body = "The following user " + username + " initiated a forgotten password request on " + System.DateTime.Now.ToString("yyyy-MM-dd") + ".";
+            var body = "<style type=\"text/css\">body{font-family:Verdana, Geneva, sans-serif;font-size:13px;line-height:19px;}table {border: solid #666 1px;font-size:13px;line-height:19px;}td {border: solid #666 1px;padding:5px;}</ style >";
+            body += "<p>The following user [" + username + "] initiated a forgotten password request on [" + System.DateTime.Now.ToString("yyyy-MM-dd") + "].</p>";
             body += " <br /><br />";
-            body += " Please use the following link to reset your password: ";
-            body += " <br />";
-            body += " <a href='" + string.Format("{0}forgotpasswordchange?guid={1}&email={2}", HttpContext.Current.Request.Url.AbsoluteUri.Replace("forgotpassword", ""), guid.ToString(), toEmail) + "'>Click here</a>";
-            body += " <br /><br />";
-            body += " If you didn’t initiate this request please use the link below to invalidate it and contact " + fromEmail;
-            body += " <br /><br />";
-            body += " This e-mail was sent on by the CISELECT application.";
+            body += " <p>  Please use the following link to reset your password:</p>";
+            body += " <p> <a href='" + string.Format("{0}forgotpasswordchange?guid={1}&email={2}", HttpContext.Current.Request.Url.AbsoluteUri.Replace("forgotpassword", ""), guid.ToString(), toEmail) + "'>Click here</a></p> ";
+            body += " <p>  If you didn&rsquo;t initiate this request please use the link  below to invalidate it and contact [ <strong>"+ fromEmail+"</strong> ].</p> " ;
+            body += " <p>  [ <strong>InvalidateLink</strong> ]</p>";
+            body += " <p><em>This e-mail was sent on by the CISELECT application.  </em></p>";
+            body += "<p><img src=\"http://tierracreative.clientpreview.agency/Images/Logo.jpg\" width=\"240\" height=\"60\"></p>";
 
             emailmodel.Subject = subject;
             emailmodel.Body = body;
@@ -218,11 +220,11 @@ namespace TierraCreative.Controllers.Utility
             if (HttpContext.Current.Request.Url.AbsoluteUri.ToLower().IndexOf("admin") != -1)
                 subject = "CISELECT – Admin Password Changed";
 
-            var body = "The following user " + username + " has changed the password associated with their account on " + System.DateTime.Now.ToString("yyyy-MM-dd") + ".";
-            body += " <br /><br />";
-            body += " If you didn’t initiate this password change please contact " + fromEmail;
-            body += " <br /><br />";
-            body += " This e-mail was sent on by the CISELECT application.";
+            var body = "<style type=\"text/css\">body{font-family:Verdana, Geneva, sans-serif;font-size:13px;line-height:19px;}table {border: solid #666 1px;font-size:13px;line-height:19px;}td {border: solid #666 1px;padding:5px;}</ style >";
+            body += "<p>The following user  <strong>" + username + " </strong> has changed the password associated with their account on <strong>" + System.DateTime.Now.ToString("yyyy-MM-dd") + "</strong> .</p>";
+            body += "<p>  If you didn&rsquo;t initiate this password change please contact <strong>" + fromEmail+ "</strong> .</p>";
+            body += "<p><em>This e-mail was sent on by the CISELECT application.  </em></p>";
+            body += "<p><img src=\"http://tierracreative.clientpreview.agency/Images/Logo.jpg\" width=\"240\" height=\"60\"></p>";
 
             emailmodel.Subject = subject;
             emailmodel.Body = body;
@@ -263,16 +265,16 @@ namespace TierraCreative.Controllers.Utility
 
             var subject = @"CISELECT – Support Request";
 
-            var body = "Support request was submitted on " + System.DateTime.Now.ToString("yyyy-MM-dd") + ".";
-            body += " <br /><br />";
-            body += string.Format("{0} [{1}]", fullame, username);
-            body += " <br /><br />";
-            body += " Support Message:";
-            body += " <br />";
-            body += message;
-            body += " <br /><br />";
+            var body = "<style type=\"text/css\">body{font-family:Verdana, Geneva, sans-serif;font-size:13px;line-height:19px;}table {border: solid #666 1px;font-size:13px;line-height:19px;}td {border: solid #666 1px;padding:5px;}</ style >";
+            body += "<p>Support request was submitted on <strong>" + System.DateTime.Now.ToString("yyyy-MM-dd") + "</strong> .</p>";
+            body += "<p>  User:</p>";
+            body += string.Format("<strong>{0}</strong> (<strong>{1}</strong>)", fullame, username);
+            body += "<p>  <strong>"+ toEmail + "</strong></p>";
+            body += "<p>  Support Message:</p>";
+            body += "<p>   <strong>"+ message + "</strong> </p>";
             DateTime dateTime = DateTime.UtcNow.Date;
-            body += " This e-mail was sent on by the CISELECT application.";
+            body += "<p><em>This e-mail was sent on by the CISELECT application.  </em></p>";
+            body += "<p><img src=\"http://tierracreative.clientpreview.agency/Images/Logo.jpg\" width=\"240\" height=\"60\"></p>";
 
             emailmodel.Subject = subject;
             emailmodel.Body = body;
@@ -313,13 +315,12 @@ namespace TierraCreative.Controllers.Utility
 
             var subject = @"CISELECT – Support Request";
 
-            var body = "Support request was submitted on " + System.DateTime.Now.ToString("yyyy-MM-dd") + ".";
-            body += " <br /><br />";
-            body += " Your Support Message:";
-            body += " <br />";
-            body += message;
-            body += " <br /><br />";
-            body += " This e-mail was sent on by the CISELECT application.";
+            var body = "<style type=\"text/css\">body{font-family:Verdana, Geneva, sans-serif;font-size:13px;line-height:19px;}table {border: solid #666 1px;font-size:13px;line-height:19px;}td {border: solid #666 1px;padding:5px;}</ style >";
+            body += "<p>Support request was submitted on <strong>" + System.DateTime.Now.ToString("yyyy-MM-dd") + "</strong>.</p>";
+            body += "<p>  Your Support Message:</p>";
+            body += "<p>   <strong>" + message + "</strong> </p>";
+            body += "<p><em>This e-mail was sent on by the CISELECT application.  </em></p>";
+            body += "<p><img src=\"http://tierracreative.clientpreview.agency/Images/Logo.jpg\" width=\"240\" height=\"60\"></p>";
 
             emailmodel.Subject = subject;
             emailmodel.Body = body;
